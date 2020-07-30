67641386e0ad32d7a5ebaaccb5c412da18b52097
yangtong <690095747@qq.com>
2020-07-30 23:40:39 +0800

    登录主界面逻辑修改

--- app/src/main/java/com/example/bds/MainActivity.java
+++ app/src/main/java/com/example/bds/MainActivity.java
@@ -22,7 +22,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,RadioGroup.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText uerName;
     private EditText password;
@@ -40,107 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        uerName = (EditText)findViewById(R.id.uerName);
-        password = (EditText)findViewById(R.id.password);
-        btLogin = (Button)findViewById(R.id.button_login);
-        btLogin.setOnClickListener(this);
-
-        initView(); //初始化组件
-        mRadioGroup.setOnCheckedChangeListener(this); //点击事件
-        fragments = getFragments(); //添加布局
-        //添加默认布局
-        normalFragment();
     }
-
-
-
-    //默认布局
-    private void normalFragment() {
-        fm=getSupportFragmentManager();
-        transaction=fm.beginTransaction();
-        fragment=fragments.get(0);
-        transaction.replace(R.id.fragment,fragment);
-        transaction.commit();
-    }
-
-    private void initView() {
-        mRadioGroup = (RadioGroup) findViewById(R.id.rg_main);
-        rb_set= (RadioButton) findViewById(R.id.rb_set);
-        rb_support= (RadioButton) findViewById(R.id.rb_support);
-        rb_help= (RadioButton) findViewById(R.id.rb_help);
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-        fm=getSupportFragmentManager();
-        transaction=fm.beginTransaction();
-        switch (checkedId){
-            case R.id.rb_set:
-                fragment=fragments.get(0);
-                transaction.replace(R.id.fragment,fragment);
-                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.rb_support:
-                fragment=fragments.get(1);
-                transaction.replace(R.id.fragment,fragment);
-                Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.rb_help:
-                fragment=fragments.get(2);
-                transaction.replace(R.id.fragment,fragment);
-                Toast.makeText(this, "Find", Toast.LENGTH_SHORT).show();
-                break;
-        }
-//        setTabState();
-        transaction.commit();
-    }
-
-    //设置选中和未选择的状态
-//    private void setTabState() {
-//        setState();
-//        setHelpState();
-//        setSupportState();
-//    }
-//
-//    private void setState() {
-//        if (rb_set.isChecked()){
-//            rb_set.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonP));
-//        }else{
-//            rb_set.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonN));
-//        }
-//    }
-//
-//    private void setHelpState() {
-//        if (rb_help.isChecked()){
-//            rb_help.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonP));
-//        }else{
-//            rb_help.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonN));
-//        }
-//    }
-//
-//    private void setSupportState() {
-//        if (rb_support.isChecked()){
-//            rb_support.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonP));
-//        }else{
-//            rb_support.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonN));
-//        }
-//    }
-
-//    private void setHomeState() {
-//        if (rb_Home.isChecked()){
-//            rb_Home.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonP));
-//        }else{
-//            rb_Home.setTextColor(ContextCompat.getColor(this,R.color.colorRadioButtonN));
-//        }
-//    }
-
-    public List<Fragment> getFragments() {
-        fragments.add(new SupportFragment());
-        fragments.add(new ConfigFragment());
-        fragments.add(new HelpFragment());
-        return fragments;
-    }
-
     @Override
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this,HomeActivity.class);
