e7ad300f33c7b6af02ce89905324bf0f8c794759
LancerWang001 <wangandong001@gmail.com>
2020-07-30 23:14:04 +0800

    cmnt

--- app/src/main/java/com/example/bds/HomeActivity.java
+++ app/src/main/java/com/example/bds/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.bds;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.home_fragment_container,new ParamsFragment())
+            .addToBackStack(null)
+            .commit();
     }
 
     @Override
